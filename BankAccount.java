bea60fafbb5aeeca04ef4caab14a7ba6a42d429d
Algorithm-Lord <raufadna@gmail.com>
2023-09-05 14:55:27 +0400

    Initial commit

---
+++ BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    private double balance;
+    private double money;
+
+    public BankAccount(double balance, double money) {
+        this.balance = balance;
+        this.money = money;
+    }
+
+    public void insufficientBalance() {
+        double totalBalance = balance - money;
+        if (balance < money) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        } else {
+            System.out.println("Operation successfully completed and your balance is : " + totalBalance + " USD");
+        }
+
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount=new BankAccount(150,100);
+        bankAccount.insufficientBalance();
+
+    }
+}
